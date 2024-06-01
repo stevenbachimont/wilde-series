9d479f07c8222c62acd4356a6fbdb7c72dbb519d
stevenbachimont <stevenbachimont@gmail.com>
2024-06-01 18:47:12 +0200

    exo 6

--- server/app/controllers/categoryActions.js
+++ server/app/controllers/categoryActions.js
@@ -1,3 +1,5 @@
+
+const tables = require("../../database/tables");
 // Some data to make the trick
 
 const categories = [
@@ -13,9 +15,11 @@ const categories = [
 
 // Declare the actions
 
-const browse = (req, res) => {
-    res.json(categories);
-};
+const browse = async (req, res) => {
+      const categoriesFromDB = await tables.category.readAll();
+
+          res.json(categoriesFromDB);
+    };
 
 const read = (req, res) => {
     const parsedId = parseInt(req.params.id, 10);
